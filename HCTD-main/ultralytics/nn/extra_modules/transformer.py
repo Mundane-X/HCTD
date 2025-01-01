1a7f779aa36c08903ed82df520688bdbb0527424
Mundane-X <48739592+Mundane-X@users.noreply.github.com>
2025-01-01 14:39:45 +0800

    Add files via upload

---
+++ HCTD-main/ultralytics/nn/extra_modules/transformer.py
@@ -0,0 +1,106 @@
+import itertools
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from functools import partial
+from einops import rearrange
+
+from ..modules.conv import Conv, autopad
+from ..modules.transformer import TransformerEncoderLayer
+from .attention import DAttention, HiLo, EfficientAdditiveAttnetion
+from .prepbn import RepBN, LinearNorm
+
+ln = nn.LayerNorm
+linearnorm = partial(LinearNorm, norm1=ln, norm2=RepBN, step=60000)
+
+__all__ = ['TransformerEncoderLayer_AdditiveTokenMixer'
+           ]
+
+class SpatialOperation(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(dim, dim, 3, 1, 1, groups=dim),
+            nn.BatchNorm2d(dim),
+            nn.ReLU(True),
+            nn.Conv2d(dim, 1, 1, 1, 0, bias=False),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return x * self.block(x)
+
+class ChannelOperation(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Conv2d(dim, dim, 1, 1, 0, bias=False),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return x * self.block(x)
+
+class AdditiveTokenMixer(nn.Module):
+    """
+    改变了proj函数的输入，不对q+k卷积，而是对融合之后的结果proj
+    """
+    def __init__(self, dim=512, attn_bias=False, proj_drop=0.):
+        super().__init__()
+        self.qkv = nn.Conv2d(dim, 3 * dim, 1, stride=1, padding=0, bias=attn_bias)
+        self.oper_q = nn.Sequential(
+            SpatialOperation(dim),
+            ChannelOperation(dim),
+        )
+        self.oper_k = nn.Sequential(
+            SpatialOperation(dim),
+            ChannelOperation(dim),
+        )
+        self.dwc = nn.Conv2d(dim, dim, 3, 1, 1, groups=dim)
+
+        self.proj = nn.Conv2d(dim, dim, 3, 1, 1, groups=dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        q, k, v = self.qkv(x).chunk(3, dim=1)
+        q = self.oper_q(q)
+        k = self.oper_k(k)
+        out = self.proj(self.dwc(q + k) * v)
+        out = self.proj_drop(out)
+        return out
+
+class TransformerEncoderLayer_AdditiveTokenMixer(nn.Module):
+    """Defines a single layer of the transformer encoder."""
+
+    def __init__(self, c1, cm=2048, num_heads=8, dropout=0.0, act=nn.GELU(), normalize_before=False):
+        """Initialize the TransformerEncoderLayer with specified parameters."""
+        super().__init__()
+        self.additivetoken = AdditiveTokenMixer(c1)
+        # Implementation of Feedforward model
+        self.fc1 = nn.Conv2d(c1, cm, 1)
+        self.fc2 = nn.Conv2d(cm, c1, 1)
+
+        self.norm1 = LayerNorm(c1)
+        self.norm2 = LayerNorm(c1)
+        self.dropout = nn.Dropout(dropout)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.act = act
+        self.normalize_before = normalize_before
+
+    def forward_post(self, src, src_mask=None, src_key_padding_mask=None, pos=None):
+        """Performs forward pass with post-normalization."""
+        src2 = self.additivetoken(src)
+        src = src + self.dropout1(src2)
+        src = self.norm1(src)
+        src2 = self.fc2(self.dropout(self.act(self.fc1(src))))
+        src = src + self.dropout2(src2)
+        return self.norm2(src)
+
+    def forward(self, src, src_mask=None, src_key_padding_mask=None, pos=None):
+        """Forward propagates the input through the encoder module."""
+        return self.forward_post(src, src_mask, src_key_padding_mask, pos)
+
+
